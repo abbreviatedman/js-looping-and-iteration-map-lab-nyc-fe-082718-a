5e36655dad0be968d997c6418dcd463f9913629d
Colin Jaffe <colin.jaffe@gmail.com>
2018-10-11 00:13:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 // Code your solution in this file.
 
 function lowerCaseDrivers(driverName) {
-  
+  return driverName.toLowerCase();
 }
 
 const drivers = ["Colin", "Herman", "Cricky", "Casey"];
 
 drivers.map(lowerCaseDrivers);
 
-func(arr[i])
+const newElement = func(arr[i]);
+newArr.push(newElement);
 
 // lowerCaseDriver('Colin')
 // lowerCaseDriver('Herman');
